9634bf8fa1ec6ca2c12ca5fc89f4cba2db62b2f4
Louis <louisjencka@gmail.com>
2014-07-01 11:30:58 -0600

    Fix cert update exec

--- site/profiles/manifests/tcc/ldap.pp
+++ site/profiles/manifests/tcc/ldap.pp
@@ -44,8 +44,9 @@ class profiles::tcc::ldap(
     }
 
     exec { "ldap-update-cert":
-        command =>  "certutil -A -n itcCA -t 'TCu,TCu,TCu' -i /etc/openldap/cacerts/tccCA.pem -d /etc/openldap/cacerts/; /sbin/reboot",
-        onlyif => "if [[ -n $(certutil -L -d /etc/openldap/cacerts | grep itcCA) ]]; then exit 1; else exit 0; fi"
+      provider => shell,
+      command =>  "/usr/bin/certutil -A -n itcCA -t 'TCu,TCu,TCu' -i /etc/openldap/cacerts/tccCA.pem -d /etc/openldap/cacerts/; /sbin/reboot",
+      onlyif => "if [[ -n $(certutil -L -d /etc/openldap/cacerts | grep itcCA) ]]; then exit 1; else exit 0; fi"
     }
   }
 }
