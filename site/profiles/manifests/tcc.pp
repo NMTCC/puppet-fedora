4c5f8c672f2ff38c349f52c69502980475c82239
dgraham <dgraham@nmt.edu>
2014-07-09 16:47:28 -0600

    updated group tcc sudoers bin list

--- site/profiles/manifests/tcc.pp
+++ site/profiles/manifests/tcc.pp
@@ -7,7 +7,7 @@ class profiles::tcc {
   # This has the same hack, plus, the arrow at the end of this line makes 'yum-makecache' run before 'yum-upgrade'; puppet is orderless otherwise.
   profiles::tcc::execnot { 'yum-makecache': command => '/bin/yum makecache', timeout => 0, onlyifnot => '/bin/yum makecache', } ->
   profiles::tcc::execnot { 'yum-update': command => '/bin/yum update -y --setopt=protected_multilib=false', timeout => 0, onlyifnot => '/bin/yum check-update', }
-  class { '::sudo': } sudo::conf { 'ucsh' : content => '%tcc ALL = (root) NOPASSWD: /fs/tcc/bin/add_user_db, /bin/arch, /fs/tcc/bin/create_dir, /usr/bin/du, /usr/bin/file, /bin/hostname, /usr/sbin/lpc, /usr/bin/lprm, /bin/ls, /fs/global/acct/bin/newacct, /bin/nice, /usr/bin/renice, /bin/ps, /usr/bin/quota, /bin/stty, /usr/sbin/tcpdump, /fs/tcc/bin/tmp_clean, /fs/tcc/bin/tmp_purge, /usr/bin/top, /usr/bin/whoami, /bin/mount, /bin/umount, /bin/uname, /usr/bin/kill, /usr/bin/killall, /usr/bin/wall, /fs/tcc/bin/acctmngr, /usr/bin/eject, /bin/chgrp, /usr/bin/halt, /usr/bin/poweroff, /usr/bin/reboot, /u/uc/bin/fix_floppy', }
+  class { '::sudo': } sudo::conf { 'ucsh' : content => '%tcc ALL = (root) NOPASSWD: /usr/bin/du, /usr/bin/file, /usr/bin/ls, /usr/bin/nice, /usr/bin/renice, /usr/bin/ps, /usr/bin/quota, /usr/bin/mount, /usr/bin/umount, /usr/bin/kill, /usr/bin/killall, /usr/bin/wall, /usr/bin/eject, /usr/bin/chgrp, /usr/sbin/halt, /usr/sbin/poweroff, /usr/sbin/reboot, /usr/sbin/shutdown, /usr/local/bin/reapply', }
   class { '::selinux': mode => 'disabled', }
   
 }
