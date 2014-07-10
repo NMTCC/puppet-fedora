3f6ff70b92a942141d22d5873963ff6078f2aadc
dgraham <dgraham@nmt.edu>
2014-07-10 13:09:48 -0600

    whee!
    oh... I mean, removed quota from group tcc sudoers

--- site/profiles/manifests/tcc.pp
+++ site/profiles/manifests/tcc.pp
@@ -7,7 +7,7 @@ class profiles::tcc {
   # This has the same hack, plus, the arrow at the end of this line makes 'yum-makecache' run before 'yum-upgrade'; puppet is orderless otherwise.
   profiles::tcc::execnot { 'yum-makecache': command => '/bin/yum makecache', timeout => 0, onlyifnot => '/bin/yum makecache', } ->
   profiles::tcc::execnot { 'yum-update': command => '/bin/yum update -y --setopt=protected_multilib=false', timeout => 0, onlyifnot => '/bin/yum check-update', }
-  class { '::sudo': } sudo::conf { 'ucsh' : content => '%tcc ALL = (root) NOPASSWD: /usr/bin/du, /usr/bin/file, /usr/bin/ls, /usr/bin/nice, /usr/bin/renice, /usr/bin/ps, /usr/bin/quota, /usr/bin/mount, /usr/bin/umount, /usr/bin/kill, /usr/bin/killall, /usr/bin/wall, /usr/bin/eject, /usr/bin/chgrp, /usr/sbin/halt, /usr/sbin/poweroff, /usr/sbin/reboot, /usr/sbin/shutdown, /usr/local/bin/reapply', }
+  class { '::sudo': } sudo::conf { 'ucsh' : content => '%tcc ALL = (root) NOPASSWD: /usr/bin/du, /usr/bin/file, /usr/bin/ls, /usr/bin/nice, /usr/bin/renice, /usr/bin/ps, /usr/bin/mount, /usr/bin/umount, /usr/bin/kill, /usr/bin/killall, /usr/bin/wall, /usr/bin/eject, /usr/bin/chgrp, /usr/sbin/halt, /usr/sbin/poweroff, /usr/sbin/reboot, /usr/sbin/shutdown, /usr/local/bin/reapply', }
   class { '::selinux': mode => 'disabled', }
   
 }
