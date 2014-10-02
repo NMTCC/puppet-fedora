e0db1b65c53d92aef41cce6b2222ff4a567230c6
dgraham <dgraham@nmt.edu>
2014-10-01 18:12:17 -0600

    stupid java

--- site/profiles/manifests/rsync/logo.pp
+++ site/profiles/manifests/rsync/logo.pp
@@ -2,12 +2,4 @@ class profiles::rsync::logo {
   rsync::get { 'logo':
     source => "update.nmt.edu::F19-logo",
     path => '/usr/local/' }
-
-  file { '/usr/local/bin/starlogo':
-    ensure => 'link',
-    target => '/usr/local/starlogo-2.2/starlogo-unix' }
-
-  file { '/usr/local/bin/netlogo':
-    ensure => 'link',
-    target => '/usr/local/netlogo-5.1.0/netlogo.sh' }
 }
