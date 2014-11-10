2fac80685a6e539f0aa90119d9b506f61f115c8f
dgraham <dgraham@nmt.edu>
2014-11-10 16:56:03 -0700

    match rsync stuff to existing naming conventions

--- site/profiles/manifests/rsync/extlinux.pp
+++ site/profiles/manifests/rsync/extlinux.pp
@@ -1,4 +1,4 @@
-class profiles::rsync::extlinux {
+class profiles::nmt::rsync::extlinux {
   rsync::get { 'extlinux':
     source => "update.nmt.edu::F19-extlinux",
     path => '/boot/extlinux/' }
--- site/profiles/manifests/rsync/logo.pp
+++ site/profiles/manifests/rsync/logo.pp
@@ -1,4 +1,4 @@
-class profiles::rsync::logo {
+class profiles::nmt::rsync::logo {
   rsync::get { 'logo':
     source => "update.nmt.edu::F19-logo",
     path => '/usr/local/' }
