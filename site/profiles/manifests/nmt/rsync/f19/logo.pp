f81a55fb9089ff5e515ec7c7e0dbaf05b195fc30
dgraham <dgraham@nmt.edu>
2014-11-19 16:42:18 -0700

    try removing rsync link option

--- site/profiles/manifests/nmt/rsync/f19/logo.pp
+++ site/profiles/manifests/nmt/rsync/f19/logo.pp
@@ -2,6 +2,5 @@ class profiles::nmt::rsync::f19::logo {
   rsync::get { 'logo':
     source => "update.nmt.edu::F19-logo",
     path => '/usr/local/',
-    recursive => true,
-    links => true }
+    recursive => true }
 }
--- site/profiles/manifests/nmt/rsync/f21/extlinux.pp
+++ site/profiles/manifests/nmt/rsync/f21/extlinux.pp
@@ -2,6 +2,5 @@ class profiles::nmt::rsync::f21::extlinux {
   rsync::get { 'extlinux':
     source => "update.nmt.edu::F21-extlinux",
     path => '/boot/extlinux/',
-    recursive => true,
-    links => true }
+    recursive => true }
 }
