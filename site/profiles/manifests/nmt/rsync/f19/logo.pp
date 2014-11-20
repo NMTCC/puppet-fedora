c1490a63d135f049972e62674bf974941285dcb7
dgraham <dgraham@nmt.edu>
2014-11-19 17:09:43 -0700

    put --links back, test adding --times

--- site/profiles/manifests/nmt/rsync/f19/logo.pp
+++ site/profiles/manifests/nmt/rsync/f19/logo.pp
@@ -2,5 +2,7 @@ class profiles::nmt::rsync::f19::logo {
   rsync::get { 'logo':
     source => "update.nmt.edu::F19-logo",
     path => '/usr/local/',
-    recursive => true }
+    recursive => true,
+    links => true,
+    times => true }
 }
--- site/profiles/manifests/nmt/rsync/f21/extlinux.pp
+++ site/profiles/manifests/nmt/rsync/f21/extlinux.pp
@@ -2,5 +2,7 @@ class profiles::nmt::rsync::f21::extlinux {
   rsync::get { 'extlinux':
     source => "update.nmt.edu::F21-extlinux",
     path => '/boot/extlinux/',
-    recursive => true }
+    recursive => true,
+    links => true,
+    times => true }
 }
