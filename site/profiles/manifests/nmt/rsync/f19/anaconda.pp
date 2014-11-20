54b4dbcc23587368d589853eb65831a30fff3f6c
dgraham <dgraham@nmt.edu>
2014-11-19 17:46:02 -0700

    moving comsol rsync away from top level

--- site/profiles/manifests/nmt/rsync/f19/anaconda.pp
+++ site/profiles/manifests/nmt/rsync/f19/anaconda.pp
@@ -1,8 +1,11 @@
 class profiles::nmt::rsync::f19::anaconda {
+
   rsync::get { 'anaconda':
     source => "update.nmt.edu::F19-anaconda/usr/local/anaconda",
     path => '/usr/local/',
     recursive => true,
     links => true,
-    times => true }
+    times => true,
+  }
+
 }
