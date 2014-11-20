c60cd2e03733cdb672b8406301ae398e790a5050
dgraham <dgraham@nmt.edu>
2014-11-19 17:20:00 -0700

    try moving anaconda rsync down from top level

--- site/profiles/manifests/nmt/rsync/f19/anaconda.pp
+++ site/profiles/manifests/nmt/rsync/f19/anaconda.pp
@@ -1,7 +1,8 @@
 class profiles::nmt::rsync::f19::anaconda {
   rsync::get { 'anaconda':
-    source => "update.nmt.edu::F19-anaconda",
-    path => '/',
+    source => "update.nmt.edu::F19-anaconda/usr/local/anaconda",
+    path => '/usr/local/',
     recursive => true,
-    links => true }
+    links => true,
+    times => true }
 }
