b0af698e8a38a504217be220ff0c2d48564dd1f4
dgraham <dgraham@nmt.edu>
2014-11-19 18:19:39 -0700

    shuffled rsync for maple, matlab, and vmd

--- site/profiles/manifests/nmt/rsync/f19/vmd.pp
+++ site/profiles/manifests/nmt/rsync/f19/vmd.pp
@@ -1,7 +1,17 @@
 class profiles::nmt::rsync::f19::vmd {
+
   rsync::get { 'vmd':
-    source => "update.nmt.edu::F19-vmd",
-    path => '/',
+    source => 'update.nmt.edu::F19-vmd/usr/local/lib/vmd',
+    path => '/usr/local/lib/',
     recursive => true,
-    links => true }
+    links => true,
+    times => true,
+  }
+
+  rsync::get { 'vmd-bin':
+    source => 'update.nmt.edu::F19-vmd/usr/local/bin/vmd',
+    path => '/usr/local/bin/',
+    times => true,
+  }
+
 }
