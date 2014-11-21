914c7f0fa7b68ea2e420b453befbd666bbde2670
Ian Neal <wopr@nmt.edu>
2014-11-20 18:19:47 -0700

    Fix kinit commands

--- site/profiles/manifests/nmt.pp
+++ site/profiles/manifests/nmt.pp
@@ -3,10 +3,10 @@
 class profiles::nmt {
 
   # This is a hack to make nodes report as unchanged; they report as changed whenever an exec is actually run.
-  profiles::nmt::execnot { 'kinit': command => '/bin/kinit -k', onlyifnot => '/bin/kinit -k', }
+  exec { 'kinit': command => '/bin/kinit -k', unless => '/bin/klist', }
   # This has the same hack, plus, the arrow at the end of this line makes 'yum-makecache' run before 'yum-upgrade'; puppet is orderless otherwise.
-  profiles::nmt::execnot { 'yum-makecache': command => '/bin/yum makecache', timeout => 0, onlyifnot => '/bin/yum makecache', } ->
-  profiles::nmt::execnot { 'yum-update': command => '/bin/yum update -y --setopt=protected_multilib=false', timeout => 0, onlyifnot => '/bin/yum check-update', }
+  exec { 'yum-makecache': command => '/bin/yum makecache', timeout => 0, unless => '/bin/yum makecache', } ->
+  exec { 'yum-update': command => '/bin/yum update -y --setopt=protected_multilib=false', timeout => 0, unless => '/bin/yum check-update', }
   class { '::sudo': } sudo::conf { 'ucsh' : content => '%tcc ALL = (root) NOPASSWD: /usr/bin/du, /usr/bin/file, /usr/bin/ls, /usr/bin/nice, /usr/bin/renice, /usr/bin/ps, /usr/bin/mount, /usr/bin/umount, /usr/bin/kill, /usr/bin/killall, /usr/bin/wall, /usr/bin/eject, /usr/bin/chgrp, /usr/sbin/halt, /usr/sbin/poweroff, /usr/sbin/reboot, /usr/sbin/shutdown, /usr/local/bin/reapply', }
   class { '::selinux': mode => 'disabled', }
   
