768566ff7a0a4228d513f415bc21be7c8144d973
dgraham <dgraham@nmt.edu>
2015-01-13 16:34:39 -0700

    Revert "temporarily disabling all but essential rsync pkgs"
    This reverts commit 3fcb5c1c466babd4187f263542941dfa515d97fc.

--- site/profiles/manifests/nmt/rsync.pp
+++ site/profiles/manifests/nmt/rsync.pp
@@ -9,14 +9,14 @@ class profiles::nmt::rsync {
       include profiles::nmt::rsync::f19::f21ks
     }
     '21': {
-#      include profiles::nmt::rsync::f19::anaconda
-#      include profiles::nmt::rsync::f21::comsol
+      include profiles::nmt::rsync::f19::anaconda
+      include profiles::nmt::rsync::f21::comsol
       include profiles::nmt::rsync::f21::extlinux
       include profiles::nmt::rsync::f21::isolinux
-#      include profiles::nmt::rsync::f19::logo
-#      include profiles::nmt::rsync::f19::maple
-#      include profiles::nmt::rsync::f19::matlab
-#      include profiles::nmt::rsync::f19::vmd
+      include profiles::nmt::rsync::f19::logo
+      include profiles::nmt::rsync::f19::maple
+      include profiles::nmt::rsync::f19::matlab
+      include profiles::nmt::rsync::f19::vmd
     }
     default: {
       warning('No rsync paths for this release version.')
