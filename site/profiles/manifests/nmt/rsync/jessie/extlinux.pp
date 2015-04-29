77720858482b43ce5b7935232820d8251b0ac2f9
dgraham <dgraham@nmt.edu>
2015-04-29 13:11:53 -0600

    also rsync memdisk; switch to jessie extlinux dir on update

--- site/profiles/manifests/nmt/rsync/jessie/extlinux.pp
+++ site/profiles/manifests/nmt/rsync/jessie/extlinux.pp
@@ -1,7 +1,7 @@
 class profiles::nmt::rsync::jessie::extlinux {
 
   rsync::get { 'extlinux':
-    source => 'update.nmt.edu::F21-extlinux',
+    source => 'update.nmt.edu::Jessie-extlinux',
     path => '/boot/extlinux/',
     recursive => true,
     links => true,
@@ -12,6 +12,12 @@ class profiles::nmt::rsync::jessie::extlinux {
     provider => shell,
     command  => 'rsync -aqc /usr/lib/syslinux/modules/bios/ /boot/extlinux/',
     onlyif   => 'test $(rsync -avnc /usr/lib/syslinux/modules/bios/ /boot/extlinux/ | grep c32 | wc -l) -gt 0',
+  }
+
+  exec { 'memdisk':
+    provider => shell,
+    command  => 'rsync -aqc /usr/lib/syslinux/memdisk /boot/extlinux/',
+    onlyif   => 'diff /usr/lib/syslinux/memdisk /boot/extlinux/memdisk',
   }
 
 }
