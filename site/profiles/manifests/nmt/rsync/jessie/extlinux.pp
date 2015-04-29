e4ca3db2101a2ed63726738b0c4dd87b60efaae8
dgraham <dgraham@nmt.edu>
2015-04-29 13:30:20 -0600

    double oops

--- site/profiles/manifests/nmt/rsync/jessie/extlinux.pp
+++ site/profiles/manifests/nmt/rsync/jessie/extlinux.pp
@@ -11,13 +11,13 @@ class profiles::nmt::rsync::jessie::extlinux {
   exec { 'extlinux-modules':
     provider => shell,
     command  => 'rsync -aqc /usr/lib/syslinux/modules/bios/ /boot/extlinux/',
-    onlyif   => 'exit $(rsync -avnc /usr/lib/syslinux/modules/bios/ /boot/extlinux/ | grep c32 | wc -l)',
+    unless   => 'exit $(rsync -avnc /usr/lib/syslinux/modules/bios/ /boot/extlinux/ | grep c32 | wc -l)',
   }
 
   exec { 'memdisk':
     provider => shell,
     command  => 'rsync -aqc /usr/lib/syslinux/memdisk /boot/extlinux/',
-    onlyif   => 'exit $(diff /usr/lib/syslinux/memdisk /boot/extlinux/memdisk | wc -l)',
+    unless   => 'exit $(diff /usr/lib/syslinux/memdisk /boot/extlinux/memdisk | wc -l)',
   }
 
 }
