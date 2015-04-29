9c6f642c169e74e03d264c52bb063da7a348c04d
dgraham <dgraham@nmt.edu>
2015-04-29 13:23:33 -0600

    simplify extlinux-modules, do memdisk correctly

--- site/profiles/manifests/nmt/rsync/jessie/extlinux.pp
+++ site/profiles/manifests/nmt/rsync/jessie/extlinux.pp
@@ -11,13 +11,13 @@ class profiles::nmt::rsync::jessie::extlinux {
   exec { 'extlinux-modules':
     provider => shell,
     command  => 'rsync -aqc /usr/lib/syslinux/modules/bios/ /boot/extlinux/',
-    onlyif   => 'test $(rsync -avnc /usr/lib/syslinux/modules/bios/ /boot/extlinux/ | grep c32 | wc -l) -gt 0',
+    onlyif   => 'rsync -avnc /usr/lib/syslinux/modules/bios/ /boot/extlinux/ | grep c32 | wc -l',
   }
 
   exec { 'memdisk':
     provider => shell,
     command  => 'rsync -aqc /usr/lib/syslinux/memdisk /boot/extlinux/',
-    onlyif   => 'diff /usr/lib/syslinux/memdisk /boot/extlinux/memdisk',
+    onlyif   => 'diff /usr/lib/syslinux/memdisk /boot/extlinux/memdisk | wc -l',
   }
 
 }
