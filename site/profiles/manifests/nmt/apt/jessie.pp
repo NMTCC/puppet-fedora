e0719a9211aa2c2da1d527d03b1d89d0a589ecf6
dgraham <dgraham@nmt.edu>
2015-05-08 18:40:04 -0600

    add puppetlabs jessie repo

--- site/profiles/manifests/nmt/apt/jessie.pp
+++ site/profiles/manifests/nmt/apt/jessie.pp
@@ -6,11 +6,22 @@ class profiles::nmt::apt::jessie {
     uri => 'https://dl.google.com/linux/linux_signing_key.pub',
   }
 
+  aptkeyfromweb { 'puppetlabs':
+    uri => 'https://apt.puppetlabs.com/pubkey.gpg',
+  }
+
   aptsource { 'google-chrome':
-    type => 'deb',
-    uri => 'http://dl.google.com/linux/chrome/deb/',
+    type  => 'deb',
+    uri   => 'http://dl.google.com/linux/chrome/deb/',
     suite => 'stable',
-    comp => ['main'],
+    comp  => ['main'],
+  }
+
+  aptsource { 'puppet-labs':
+    type  => 'deb',
+    uri   => 'https://apt.puppetlabs.com/',
+    suite => 'jessie',
+    comp  => ['main', 'dependencies'],
   }
 
 }
