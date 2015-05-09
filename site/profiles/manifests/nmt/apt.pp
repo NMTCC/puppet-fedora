d4ef4c8de78a2ed04d44aa85ebc248ce5ad975fd
dgraham <dgraham@nmt.edu>
2015-05-08 18:09:50 -0600

    setup apt sources; use this to add google chrome repo

--- site/profiles/manifests/nmt/apt.pp
+++ site/profiles/manifests/nmt/apt.pp
@@ -2,6 +2,30 @@
 
 class profiles::nmt::apt {
 
+  define aptsource ($type, $uri, $suite, $comp) {
+    file { "/etc/apt/sources.list.d/${title}.list" :
+      ensure  => 'file',
+      content => "${type} ${uri} ${suite} " + join($comp, ' '),
+      group   => 'root',
+      mode    => '0644',
+      owner   => 'root',
+    }
+  }
+
+  define rmaptsource {
+    file { "/etc/apt/sources.list.d/${title}.list" :
+      ensure => 'absent',
+      force  => 'true',
+      backup => 'false',
+    }
+  }
+
+  define aptkeyfromweb ($uri) {
+    provider => 'shell',
+    command  => "wget -q -O- ${uri} | apt-key --keyring /etc/apt/trusted.gpg.d/${title}.gpg add -",
+    unless   => "test -e /etc/apt/trusted.gpg.d/${title}.gpg",
+  }
+
   case $::operatingsystemmajrelease {
     '8': {
       class { "profiles::nmt::apt::jessie": }
--- site/profiles/manifests/nmt/apt/jessie.pp
+++ site/profiles/manifests/nmt/apt/jessie.pp
@@ -2,6 +2,15 @@
 
 class profiles::nmt::apt::jessie {
 
+  aptkeyfromweb { 'google-linux':
+    uri => 'https://dl.google.com/linux/linux_signing_key.pub',
+  }
 
+  aptsource { 'google-chrome':
+    type => 'deb',
+    uri => 'http://dl.google.com/linux/chrome/deb/',
+    suite => 'stable',
+    comp => ['main'],
+  }
 
 }
