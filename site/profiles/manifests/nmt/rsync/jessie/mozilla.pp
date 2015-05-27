ada30b05a040958699bbd8ae83aeefa596d76a80
dgraham <dgraham@nmt.edu>
2015-05-26 18:04:19 -0600

    install thunderbird and firefox icons

--- site/profiles/manifests/nmt/rsync/jessie/mozilla.pp
+++ site/profiles/manifests/nmt/rsync/jessie/mozilla.pp
@@ -30,4 +30,24 @@ class profiles::nmt::rsync::jessie::mozilla {
     times  => true,
   }
 
+  define stupidicontricks {
+    rsync::get { "firefox-${title}":
+      source => "update.nmt.edu::Jessie-mozilla/icons/hicolor/${title}/apps/firefox.png",
+      path   => "/usr/share/icons/hicolor/${title}/apps/",
+      times  => true,
+    }
+    rsync::get { "thunderbird-${title}":
+      source => "update.nmt.edu::Jessie-mozilla/icons/hicolor/${title}/apps/thunderbird.png",
+      path   => "/usr/share/icons/hicolor/${title}/apps/",
+      times  => true,
+    }
+  }
+
+  stupidicontricks { '16x16' }
+  stupidicontricks { '22x22' }
+  stupidicontricks { '24x24' }
+  stupidicontricks { '32x32' }
+  stupidicontricks { '48x48' }
+  stupidicontricks { '256x256' }
+
 }
