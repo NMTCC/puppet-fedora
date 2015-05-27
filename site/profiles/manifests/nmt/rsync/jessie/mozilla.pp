d3e5b51745f7fb5e402001599f7d43485d3e36f0
dgraham <dgraham@nmt.edu>
2015-05-27 13:31:20 -0600

    remove lxde mozilla icons

--- site/profiles/manifests/nmt/rsync/jessie/mozilla.pp
+++ site/profiles/manifests/nmt/rsync/jessie/mozilla.pp
@@ -11,7 +11,7 @@ class profiles::nmt::rsync::jessie::mozilla {
 
   rsync::get { 'firefox-desktop':
     source => 'update.nmt.edu::Jessie-mozilla/firefox.desktop',
-    path   => '/usr/share/applications/',
+    path   => '/usr/local/share/applications/',
     times  => true,
   }
 
@@ -26,18 +26,18 @@ class profiles::nmt::rsync::jessie::mozilla {
 
   rsync::get { 'thunderbird-desktop':
     source => 'update.nmt.edu::Jessie-mozilla/thunderbird.desktop',
-    path   => '/usr/share/applications/',
+    path   => '/usr/local/share/applications/',
     times  => true,
   }
 
   define stupidicontricks {
     rsync::get { "firefox-${title}":
-      source => "update.nmt.edu::Jessie-mozilla/icons/${title}/firefox-official.png",
+      source => "update.nmt.edu::Jessie-mozilla/icons/${title}/firefox.png",
       path   => "/usr/share/icons/hicolor/${title}/apps/",
       times  => true,
     }
     rsync::get { "thunderbird-${title}":
-      source => "update.nmt.edu::Jessie-mozilla/icons/${title}/thunderbird-official.png",
+      source => "update.nmt.edu::Jessie-mozilla/icons/${title}/thunderbird.png",
       path   => "/usr/share/icons/hicolor/${title}/apps/",
       times  => true,
     }
@@ -49,5 +49,35 @@ class profiles::nmt::rsync::jessie::mozilla {
   stupidicontricks { '32x32': }
   stupidicontricks { '48x48': }
   stupidicontricks { '256x256': }
+
+  define morestupidicontricks {
+    file { "/usr/share/icons/nuoveXT2/${title}/apps/firefox.png":
+      ensure => 'absent',
+      backup => 'false',
+    }
+    file { "/usr/share/icons/nuoveXT2/${title}/apps/mozilla-firefox.png":
+      ensure => 'absent',
+      backup => 'false',
+    }
+    file { "/usr/share/icons/nuoveXT2/${title}/apps/thunderbird.png":
+      ensure => 'absent',
+      backup => 'false',
+    }
+    file { "/usr/share/icons/nuoveXT2/${title}/apps/mozilla-thunderbird.png":
+      ensure => 'absent',
+      backup => 'false',
+    }
+  }
+
+  morestupidicontricks { '16x16': }
+  morestupidicontricks { '22x22': }
+  morestupidicontricks { '24x24': }
+  morestupidicontricks { '32x32': }
+  morestupidicontricks { '36x36': }
+  morestupidicontricks { '48x48': }
+  morestupidicontricks { '64x64': }
+  morestupidicontricks { '72x72': }
+  morestupidicontricks { '96x96': }
+  morestupidicontricks { '128x128': }
 
 }
