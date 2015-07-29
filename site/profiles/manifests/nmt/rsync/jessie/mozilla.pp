343611fc9dc6fd257965354a4b1cded8bd36a3fd
dgraham <dgraham@nmt.edu>
2015-07-29 17:59:18 -0600

    link firefox and thunderbird into path, plush better deps

--- site/profiles/manifests/nmt/rsync/jessie/mozilla.pp
+++ site/profiles/manifests/nmt/rsync/jessie/mozilla.pp
@@ -13,12 +13,20 @@ class profiles::nmt::rsync::jessie::mozilla {
     source => 'update.nmt.edu::Jessie-mozilla/firefox.desktop',
     path   => '/usr/local/share/applications/',
     times  => true,
+    require => Rsync::Get['firefox'],
   }
 
   rsync::get { 'firefox-icon':
     source => 'update.nmt.edu::Jessie-mozilla/firefox.png',
     path   => '/usr/share/icons/hicolor/256x256/apps/',
     times  => true,
+    require => Rsync::Get['firefox'],
+  }
+
+  file { '/usr/bin/firefox.real':
+    ensure  => 'link',
+    target  => '/usr/local/firefox/firefox',
+    require => Rsync::Get['firefox'],
   }
 
   rsync::get { 'thunderbird':
@@ -34,12 +42,20 @@ class profiles::nmt::rsync::jessie::mozilla {
     source => 'update.nmt.edu::Jessie-mozilla/thunderbird.desktop',
     path   => '/usr/local/share/applications/',
     times  => true,
+    require => Rsync::Get['thunderbird'],
   }
 
   rsync::get { 'thunderbird-icon':
     source => 'update.nmt.edu::Jessie-mozilla/thunderbird.png',
     path   => '/usr/share/icons/hicolor/256x256/apps/',
     times  => true,
+    require => Rsync::Get['thunderbird'],
+  }
+
+  file { '/usr/bin/thunderbird':
+    ensure  => 'link',
+    target  => '/usr/local/thunderbird/thunderbird',
+    require => Rsync::Get['thunderbird'],
   }
 
 }
