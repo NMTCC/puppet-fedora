2c73fb1f279dbdde44877bb598e653a0f80f5272
dgraham <dgraham@nmt.edu>
2015-09-09 17:42:04 -0600

    cron not always broken, evidently

--- site/profiles/manifests/nmt/fix82.pp
+++ site/profiles/manifests/nmt/fix82.pp
@@ -4,8 +4,14 @@ class profiles::nmt::fix82 {
 
   exec { 'fix82':
     provider => shell,
-    command  => 'dpkg --configure -a; apt-get -y install --reinstall cron',
+    command  => 'dpkg --configure -a',
+  }
+
+  exec { 'fixcron':
+    provider => shell,
+    command  => 'apt-get -y install --reinstall cron',
     unless   => 'exit $(dpkg -s cron | grep reinstreq | wc -l)',
+    require  => Exec['fix82'],
   }
 
 }
