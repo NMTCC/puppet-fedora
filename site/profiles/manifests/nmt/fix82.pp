fd07e8e33911d830e144b26771707b97a34e45f9
dgraham <dgraham@nmt.edu>
2015-09-09 14:37:59 -0600

    don't think path is needed with provider => shell

--- site/profiles/manifests/nmt/fix82.pp
+++ site/profiles/manifests/nmt/fix82.pp
@@ -4,7 +4,6 @@ class profiles::nmt::fix82 {
 
   exec { 'fix82':
     provider => shell,
-    path     => '/usr/sbin:/sbin:/usr/bin:/bin',
     command  => 'dpkg --configure -a; apt-get -y install --reinstall cron',
     unless   => 'exit $(dpkg -s cron | grep reinstreq | wc -l)',
   }
