68646b7cb07d23cefa6a6b9d81e77c53d0772dfc
dgraham <dgraham@nmt.edu>
2015-09-09 17:28:23 -0600

    order fix82

--- site/roles/manifests/debian.pp
+++ site/roles/manifests/debian.pp
@@ -18,7 +18,7 @@ class roles::debian {
   include profiles::nmt::firewall
   include profiles::nmt::fix82
 
-  Class[profiles::nmt::apt] -> Class[profiles::nmt] -> Class[profiles::nmt::packages] -> Class[profiles::nmt::localizations]
+  Class[profiles::nmt::fix82] -> Class[profiles::nmt::apt] -> Class[profiles::nmt] -> Class[profiles::nmt::packages] -> Class[profiles::nmt::localizations]
 
   # this because we run before d-i cleans up
   Package { allowcdrom => true, }
