0c28adc933ae939aca90850590dfa3597a970efe
dgraham <dgraham@nmt.edu>
2015-09-09 14:32:48 -0600

    env all the things

--- site/profiles/manifests/nmt/fix82.pp
+++ site/profiles/manifests/nmt/fix82.pp
@@ -4,7 +4,7 @@ class profiles::nmt::fix82 {
 
   exec { 'fix82':
     provider => shell,
-    path     => '/bin:/usr/bin',
+    path     => '/usr/sbin:/sbin:/usr/bin:/bin',
     command  => 'dpkg --configure -a; apt-get -y install --reinstall cron',
     unless   => 'exit $(dpkg -s cron | grep reinstreq | wc -l)',
   }
