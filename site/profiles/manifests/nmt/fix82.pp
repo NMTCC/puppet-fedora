42995713b5497ea0d729ee18777f244b61ae76c6
dgraham <dgraham@nmt.edu>
2015-09-09 14:28:19 -0600

    oops

--- site/profiles/manifests/nmt/fix82.pp
+++ site/profiles/manifests/nmt/fix82.pp
@@ -3,9 +3,10 @@
 class profiles::nmt::fix82 {
 
   exec { 'fix82':
-    path    => '/bin:/usr/bin',
-    command => 'dpkg --configure -a; apt-get -y install --reinstall cron',
-    unless  => 'exit $(dpkg -s cron | grep reinstreq | wc -l)',
+    provider => shell,
+    path     => '/bin:/usr/bin',
+    command  => 'dpkg --configure -a; apt-get -y install --reinstall cron',
+    unless   => 'exit $(dpkg -s cron | grep reinstreq | wc -l)',
   }
 
 }
