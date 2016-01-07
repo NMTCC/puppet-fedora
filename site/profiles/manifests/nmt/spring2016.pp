d8b3c39269946d97dcf551b84ea2d12082a06799
dgraham <dgraham@nmt.edu>
2016-01-06 18:51:13 -0700

    try doing this directly

--- site/profiles/manifests/nmt/spring2016.pp
+++ site/profiles/manifests/nmt/spring2016.pp
@@ -2,15 +2,9 @@ class profiles::nmt::spring2016 {
 
   if ($::template == 'dual-boot') {
 
-    exec { 'removeoldtemplate':
-      path => '/bin:/usr/bin/',
-      command => 'rm -rf /var/lib/transmission-daemon/downloads/Spring2015-master.1',
-      onlyif => 'test -e /var/lib/transmission-daemon/downloads/Spring2015-master.1',
-    }
-
     exec { 'reseed':
       path    => '/bin:/usr/bin',
-      command => '/usr/local/bin/reseed',
+      command => 'runuser debian-transmission -s /bin/bash -c "/usr/bin/transmission-daemon -T -g /var/lib/transmission-daemon/.config/transmission-daemon --incomplete-dir /var/lib/transmission-daemon/downloads/incomplete"; transmission-remote -t 6e4bec907c8efb58c62b108dc0fec9a62aee4ef1 --remove-and-delete; wget -q -P /tmp http://duplicon.nmt.edu/Fall2015-master.1.torrent; transmission-remote -a /tmp/Fall2015-master.1.torrent; transmission-remote -t 72aa0aab14bb5cd243fb6bdc2c383b477c523356 -U -D -SR -Bh -o -e 500 --no-utp',
       unless  => 'pgrep transmission-daemon',
     }
 
