d1918ff62f23bece5109de41285b166dda562dd1
dgraham <dgraham@nmt.edu>
2016-01-06 18:58:43 -0700

    switch to shell

--- site/profiles/manifests/nmt/spring2016.pp
+++ site/profiles/manifests/nmt/spring2016.pp
@@ -2,16 +2,10 @@ class profiles::nmt::spring2016 {
 
   if ($::template == 'dual-boot') {
 
-    exec { 'removeoldtemplate':
-      path => '/bin:/usr/bin/',
-      command => 'rm -rf /var/lib/transmission-daemon/downloads/Spring2015-master.1',
-      onlyif => 'test -e /var/lib/transmission-daemon/downloads/Spring2015-master.1',
-    }
-
     exec { 'reseed':
-      path    => '/bin:/usr/bin',
-      command => '/usr/local/bin/reseed',
-      unless  => 'pgrep transmission-daemon',
+      provider => shell,
+      command  => '/usr/local/bin/reseed',
+      unless   => 'pgrep transmission-daemon',
     }
 
   }
