80089d984bfd1e9e866b72bbd2f59a184f9f6d33
dgraham <dgraham@nmt.edu>
2016-01-14 19:10:18 -0700

    don't look

--- site/profiles/manifests/nmt/spring2016.pp
+++ site/profiles/manifests/nmt/spring2016.pp
@@ -1,11 +1,15 @@
 class profiles::nmt::spring2016 {
 
-  if ($::template == 'dual-boot') and ($::blockdevice_sda_size > 500107862016) {
+  if $::blockdevice_sda_size {
 
-    exec { 'reseed':
-      provider => shell,
-      command  => '/usr/local/bin/reseed',
-      unless   => 'pgrep transmission',
+    if ($::template == 'dual-boot') and ($::blockdevice_sda_size > 500107862016) {
+
+      exec { 'reseed':
+        provider => shell,
+        command  => '/usr/local/bin/reseed',
+        unless   => 'pgrep transmission',
+      }
+
     }
 
   }
