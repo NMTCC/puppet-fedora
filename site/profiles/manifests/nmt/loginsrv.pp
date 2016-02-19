768e6dbdecc018042c1f0ea31a393ab0009f2e08
dgraham <dgraham@nmt.edu>
2016-02-19 13:47:06 -0700

    oops

--- site/profiles/manifests/nmt/loginsrv.pp
+++ site/profiles/manifests/nmt/loginsrv.pp
@@ -9,12 +9,13 @@ class profiles::nmt::loginsrv {
         provider => 'apt',
       }
 
-      file { '/etc/fail2ban/fail.d/local.conf':
+      file { '/etc/fail2ban/jail.d/local.conf':
         ensure  => 'file',
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
         content => "[DEFAULT]\nbantime = 86400\nbanaction = firewallcmd-ipset\nbackend = systemd\nsender = fail2ban@rainbow.nmt.edu\ndestemail = tcc-fail2ban@nmt.edu\nignoreip = 127.0.0.1/8 129.138.0.0/16\nmaxretry = 10\nusedns = no\n\n[sshd]\nenabled = true",
+        require => Package['fail2ban'],
       }
 
     }
