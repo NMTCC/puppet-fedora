e1dcdeca0245be0375063be5eab33cb2df388dd9
dgraham <dgraham@nmt.edu>
2016-02-19 13:53:38 -0700

    eh

--- site/profiles/manifests/nmt/loginsrv.pp
+++ site/profiles/manifests/nmt/loginsrv.pp
@@ -14,7 +14,7 @@ class profiles::nmt::loginsrv {
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => "[DEFAULT]\nbantime = 86400\nbanaction = firewallcmd-ipset\nbackend = systemd\nsender = fail2ban@rainbow.nmt.edu\ndestemail = tcc-fail2ban@nmt.edu\nignoreip = 127.0.0.1/8 129.138.0.0/16\nmaxretry = 10\nusedns = no\n\n[sshd]\nenabled = true",
+        content => "[DEFAULT]\nbantime = 86400\nbanaction = firewallcmd-ipset\nbackend = systemd\nsender = fail2ban@rainbow.nmt.edu\ndestemail = tcc-fail2ban@nmt.edu\nignoreip = 127.0.0.1/8 129.138.0.0/16\nmaxretry = 10\nusedns = no\n\n[sshd]\nenabled = true\n",
         require => Package['fail2ban'],
       }
 
