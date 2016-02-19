d1165c4e9f9111a00b1c7c6b7f2b1c4dec98afa6
dgraham <dgraham@nmt.edu>
2016-02-19 13:40:36 -0700

    setup fail2ban on login servers

---
+++ site/profiles/manifests/nmt/loginsrv.pp
@@ -0,0 +1,26 @@
+class profiles::nmt::loginsrv {
+
+  case $::hostname {
+
+    'rainbow', 'login': {
+
+      package { 'fail2ban':
+        ensure   => 'installed',
+        provider => 'apt',
+      }
+
+      file { '/etc/fail2ban/fail.d/local.conf':
+        ensure  => 'file',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => "[DEFAULT]\nbantime = 86400\nbanaction = firewallcmd-ipset\nbackend = systemd\nsender = fail2ban@rainbow.nmt.edu\ndestemail = tcc-fail2ban@nmt.edu\nignoreip = 127.0.0.1/8 129.138.0.0/16\nmaxretry = 10\nusedns = no\n\n[sshd]\nenabled = true",
+      }
+
+    }
+
+    default: { $dummy = [ 'Not a login server.', ] }
+
+  }
+
+}
