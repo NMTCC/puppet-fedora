9a8ee00e79d775698431d030cf178862f58522ba
dgraham <dgraham@nmt.edu>
2016-03-17 15:37:28 -0600

    enable fail2ban recidive

--- site/profiles/manifests/nmt/loginsrv.pp
+++ site/profiles/manifests/nmt/loginsrv.pp
@@ -14,14 +14,24 @@ class profiles::nmt::loginsrv {
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => "[DEFAULT]\nbantime = 86400\nbanaction = firewallcmd-ipset\nbackend = systemd\nsender = fail2ban@nmt.edu\ndestemail = tcc-fail2ban@nmt.edu\nignoreip = 127.0.0.1/8 129.138.0.0/16\nmaxretry = 10\naction = %(action_mwl)s\nusedns = no\n\n[sshd]\nenabled = true\n",
+        content => "[DEFAULT]\nbanaction = firewallcmd-ipset\nbackend = systemd\nsender = fail2ban@nmt.edu\ndestemail = tcc-fail2ban@nmt.edu\nignoreip = 127.0.0.1/8 129.138.0.0/16\naction = %(action_mwl)s\nusedns = no\n\n[sshd]\nenabled = true\nbantime = 86400\nmaxretry = 10\n\n[recidive]\nenabled = true\nfindtime = 604800\nmaxretry = 2\n",
         require => Package['fail2ban'],
+        notify  => Service['fail2ban'],
+      }
+
+      file { '/etc/fail2ban/fail2ban.d/local/conf':
+        ensure  => 'file',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => "[Definition]\ndbpurgeage = 1296400\n",
+        require => Package['fail2ban'],
+        notify  => Service['fail2ban'],
       }
 
       service { 'fail2ban':
         ensure    => 'running',
         enable    => 'true',
-        subscribe => File['/etc/fail2ban/jail.d/local.conf'],
       }
 
       exec { 'pam_tally2':
@@ -32,7 +42,7 @@ class profiles::nmt::loginsrv {
 
     }
 
-    default: { $dummy = [ 'Not a login server.', ] }
+    default: { notice("Not a login server.") }
 
   }
 
