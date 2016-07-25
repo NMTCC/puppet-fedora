0dea875119d7ece69dcd66a82077f28127c33c5b
dgraham <dgraham@nmt.edu>
2016-07-25 13:13:07 -0600

    switch to maple 2016

--- site/profiles/manifests/nmt/rsync/jessie/maple.pp
+++ site/profiles/manifests/nmt/rsync/jessie/maple.pp
@@ -1,13 +1,13 @@
 class profiles::nmt::rsync::jessie::maple {
 
-  exec { 'maple18':
+  exec { 'maple2015':
     path => '/bin:/usr/bin/',
-    command => 'rm -rf /usr/local/maple18',
-    onlyif => 'test -e /usr/local/maple18',
+    command => 'rm -rf /usr/local/maple2015',
+    onlyif => 'test -e /usr/local/maple2015',
   }
 
   rsync::get { 'maple':
-    source => 'duplicon.nmt.edu::Jessie-maple/maple2015',
+    source => 'duplicon.nmt.edu::Jessie-maple/maple2016',
     path => '/usr/local/',
     recursive => true,
     links => true,
@@ -15,20 +15,20 @@ class profiles::nmt::rsync::jessie::maple {
   }
 
   rsync::get { 'maple-desktop':
-    source => 'duplicon.nmt.edu::Jessie-maple/maple.desktop',
-    path => '/usr/local/share/applications/',
+    source => 'duplicon.nmt.edu::Jessie-maple/maple2016.desktop',
+    path => '/usr/local/share/applications/maple.desktop',
     times => true,
   }
 
   file { '/usr/local/bin/maple':
     ensure => 'link',
-    target => '/usr/local/maple2015/bin/maple',
+    target => '/usr/local/maple2016/bin/maple',
     require => Rsync::Get['maple'],
   }
 
   file { '/usr/local/bin/xmaple':
     ensure => 'link',
-    target => '/usr/local/maple2015/bin/xmaple',
+    target => '/usr/local/maple2016/bin/xmaple',
     require => Rsync::Get['maple'],
   }
 
