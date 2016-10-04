07f1dbeafde2c2931de68eb6c73033e10d9e818b
dgraham <dgraham@nmt.edu>
2016-10-03 19:09:27 -0600

    change splashscreen fact to use local dir

--- dist/info/lib/facter/splashscreen.rb
+++ dist/info/lib/facter/splashscreen.rb
@@ -2,7 +2,7 @@ Facter.add(:splashscreen) do
   confine :kernel => "Linux"
 
   setcode do
-    splashes = Dir.glob("/fs/tcc/wallpapers/#{Facter.value(:hostname)}.*")
+    splashes = Dir.glob("/usr/share/backgrounds/#{Facter.value(:hostname)}.*")
     if splashes == []
       result = '/usr/share/backgrounds/itcjessie.png'
     else
