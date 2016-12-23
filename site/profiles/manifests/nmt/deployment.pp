2ccb3ccbdf7961ce22521b2da06e9f80245124a9
dgraham <dgraham@nmt.edu>
2016-12-22 22:15:32 -0700

    prepare for s17 deployment

--- site/profiles/manifests/nmt/deployment.pp
+++ site/profiles/manifests/nmt/deployment.pp
@@ -12,16 +12,28 @@ class profiles::nmt::deployment {
     onlyif  => 'test -e /var/lib/transmission-daemon/downloads/Spring2016-master.1',
   }
 
+  exec { 'Fall2016-1':
+    path    => '/bin:/usr/bin',
+    command => 'rm -rf /var/lib/transmission-daemon/downloads/Fall2016-master.1',
+    onlyif  => 'test -e /var/lib/transmission-daemon/downloads/Fall2016-master.1',
+  }
+
+  exec { 'Fall2016-2':
+    path    => '/bin:/usr/bin',
+    command => 'rm -rf /var/lib/transmission-daemon/downloads/Fall2016-master.2',
+    onlyif  => 'test -e /var/lib/transmission-daemon/downloads/Fall2016-master.2',
+  }
+
   if $::blockdevice_sda_size {
 
     if ($::template == 'dual-boot') and ($::blockdevice_sda_size > 500107862016) {
 
-#      exec { 'reseed':
-#        provider => shell,
-#        command  => '/usr/local/bin/reseed',
-#        unless   => 'pgrep transmission',
-#        require  => File['reseed'],
-#      }
+      exec { 'reseed':
+        provider => shell,
+        command  => '/usr/local/bin/reseed',
+        unless   => 'pgrep transmission',
+        require  => File['reseed'],
+      }
 
     }
 
