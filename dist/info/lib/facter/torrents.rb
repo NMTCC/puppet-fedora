f8c91ef84416648346a58058f3282ce6eb9a9ce3
dgraham <dgraham@nmt.edu>
2017-01-03 17:25:01 -0700

    glob correctly and get basenames

--- dist/info/lib/facter/torrents.rb
+++ dist/info/lib/facter/torrents.rb
@@ -12,7 +12,9 @@ Facter.add(:torrentscomplete) do
   confine :kernel => "Linux"
 
   setcode do
-    torrents = Dir.glob('/var/lib/transmission-daemon/downloads')
+    Dir.chdir('/var/lib/transmission-daemon/downloads') do
+      torrents = Dir.glob('*')
+    end
     torrents.delete('incomplete')
     result = torrents
   end
