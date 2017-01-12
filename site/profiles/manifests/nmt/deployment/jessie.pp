44d5b994dcbf8de042b8db2ff5a63d11cc78342e
dgraham <dgraham@nmt.edu>
2017-01-12 13:35:11 -0700

    use single config file for all deployment scripts

--- site/profiles/manifests/nmt/deployment/jessie.pp
+++ site/profiles/manifests/nmt/deployment/jessie.pp
@@ -17,11 +17,25 @@ class profiles::nmt::deployment::jessie {
     'c51a49547a5122c8f73c322b63fdf4c851f2ff05',
   ]
 
-  $template = 'Spring2017-master.2'
+  # deployment configuration
+  $semester = 'Spring2017'
+  $branch = 'master'
+  $build = '2'
+  $template = "${semester}-${branch}.${build}"
   $torrent = '71d1caa7d8a882ecfd3c75db7775fd24b3a2f408'
+  $preseed = 'http://duplicon.nmt.edu/preseed/jessie'
+  $syncpre = 'http://template.nmt.edu/images/Spring2017'
 
   rmtemplate { $oldtemplates: }
   rmtorrent { $oldtorrents: }
   gettorrent { $template: hash => $torrent, }
+
+  file { '/usr/local/etc/template.conf':
+    ensure  => present,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('profiles/template.conf.erb'),
+  }
 
 }
