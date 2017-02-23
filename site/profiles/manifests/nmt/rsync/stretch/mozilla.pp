d0ff6b2c0a5a96675a1e3ccebe26d9e67e7865ff
dgraham <dgraham@nmt.edu>
2017-02-22 17:02:21 -0700

    update mozilla.pp to match overlays

--- site/profiles/manifests/nmt/rsync/stretch/mozilla.pp
+++ site/profiles/manifests/nmt/rsync/stretch/mozilla.pp
@@ -10,20 +10,20 @@ class profiles::nmt::rsync::stretch::mozilla {
   }
 
   rsync::get { 'firefox-desktop':
-    source => 'duplicon.nmt.edu::Stretch-mozilla/firefox.desktop',
+    source => 'duplicon.nmt.edu::Stretch-mozilla/mozilla-firefox.desktop',
     path   => '/usr/local/share/applications/',
     times  => true,
     require => Rsync::Get['firefox'],
   }
 
   rsync::get { 'firefox-icon':
-    source => 'duplicon.nmt.edu::Stretch-mozilla/firefox.png',
-    path   => '/usr/share/icons/hicolor/256x256/apps/',
+    source => 'duplicon.nmt.edu::Stretch-mozilla/mozilla-firefox.png',
+    path   => '/usr/share/pixmaps/',
     times  => true,
     require => Rsync::Get['firefox'],
   }
 
-  file { '/usr/bin/firefox.real':
+  file { '/usr/bin/firefox-stable':
     ensure  => 'link',
     target  => '/usr/local/firefox/firefox',
     require => Rsync::Get['firefox'],
@@ -39,15 +39,15 @@ class profiles::nmt::rsync::stretch::mozilla {
   }
 
   rsync::get { 'thunderbird-desktop':
-    source => 'duplicon.nmt.edu::Stretch-mozilla/thunderbird.desktop',
+    source => 'duplicon.nmt.edu::Stretch-mozilla/mozilla-thunderbird.desktop',
     path   => '/usr/local/share/applications/',
     times  => true,
     require => Rsync::Get['thunderbird'],
   }
 
   rsync::get { 'thunderbird-icon':
-    source => 'duplicon.nmt.edu::Stretch-mozilla/thunderbird.png',
-    path   => '/usr/share/icons/hicolor/256x256/apps/',
+    source => 'duplicon.nmt.edu::Stretch-mozilla/mozilla-thunderbird.png',
+    path   => '/usr/share/pixmaps/',
     times  => true,
     require => Rsync::Get['thunderbird'],
   }
@@ -56,6 +56,35 @@ class profiles::nmt::rsync::stretch::mozilla {
     ensure  => 'link',
     target  => '/usr/local/thunderbird/thunderbird',
     require => Rsync::Get['thunderbird'],
+  }
+
+  rsync::get { 'seamonkey':
+    source    => 'duplicon.nmt.edu::Stretch-mozilla/seamonkey',
+    path      => '/usr/local/',
+    recursive => true,
+    links     => true,
+    times     => true,
+    purge     => true,
+  }
+
+  rsync::get { 'seamonkey-desktop':
+    source => 'duplicon.nmt.edu::Stretch-mozilla/mozilla-seamonkey.desktop',
+    path   => '/usr/local/share/applications/',
+    times  => true,
+    require => Rsync::Get['seamonkey'],
+  }
+
+  rsync::get { 'seamonkey-icon':
+    source => 'duplicon.nmt.edu::Stretch-mozilla/mozilla-seamonkey.png',
+    path   => '/usr/share/pixmaps/',
+    times  => true,
+    require => Rsync::Get['seamonkey'],
+  }
+
+  file { '/usr/bin/seamonkey':
+    ensure  => 'link',
+    target  => '/usr/local/seamonkey/seamonkey',
+    require => Rsync::Get['seamonkey'],
   }
 
 }
