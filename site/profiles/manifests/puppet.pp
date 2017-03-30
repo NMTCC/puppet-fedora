fb643f2756125cf0246b9dd9d0c0372e2b66d33c
dgraham <dgraham@nmt.edu>
2017-03-30 13:27:47 -0600

    stop changing puppetdb config on puppet

--- site/profiles/manifests/puppet.pp
+++ site/profiles/manifests/puppet.pp
@@ -3,11 +3,11 @@
 class profiles::puppet {
   
 #  class { 'puppetdb': listen_address => '0.0.0.0', }
-  class { 'puppetdb::master::config':
-    restart_puppet  => false,
-    puppetdb_server => 'puppetdb.nmt.edu',
-    puppetdb_port   => 8081,
-  }
+#  class { 'puppetdb::master::config':
+#    restart_puppet  => false,
+#    puppetdb_server => 'puppetdb.nmt.edu',
+#    puppetdb_port   => 8081,
+#  }
 
   cron { "puppet clean reports":
     command => 'cd /var/lib/puppet/reports && find . -type f -name \*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f',
