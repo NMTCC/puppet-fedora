472578633d1159c2107e454abe998703badf9307
dgraham <dgraham@nmt.edu>
2017-05-22 17:21:43 -0600

    use versions for iris sw

--- site/profiles/manifests/nmt/rsync/jessie/iris.pp
+++ site/profiles/manifests/nmt/rsync/jessie/iris.pp
@@ -1,15 +1,27 @@
 class profiles::nmt::rsync::jessie::iris {
 
-  rsync::get { 'cwp':
-    source => 'duplicon.nmt.edu::Jessie-iris/cwp',
+  exec { 'cwp-44':
+    path    => '/bin:/usr/bin',
+    command => 'rm -rf /usr/local/cwp',
+    onlyif  => 'test -e /usr/local/cwp',
+  }
+
+  rsync::get { 'cwp-44r5':
+    source => 'duplicon.nmt.edu::Jessie-iris/cwp-44r5',
     path => '/usr/local/',
     recursive => true,
     links => true,
     times => true,
   }
 
-  rsync::get { 'oss':
-    source => 'duplicon.nmt.edu::Jessie-iris/oss',
+  exec { 'oss':
+    path    => '/bin:/usr/bin',
+    command => 'rm -rf /usr/local/oss',
+    onlyif  => 'test -e /usr/local/oss',
+  }
+
+  rsync::get { 'oss-2.03':
+    source => 'duplicon.nmt.edu::Jessie-iris/oss-2.03',
     path => '/usr/local/',
     recursive => true,
     links => true,
@@ -20,14 +32,14 @@ class profiles::nmt::rsync::jessie::iris {
     source => 'duplicon.nmt.edu::Jessie-iris/iris.sh',
     path => '/etc/profile.d/',
     times => true,
-    require => [ Rsync::Get['cwp'], Rsync::Get['oss'] ],
+    require => [ Rsync::Get['cwp-44r5'], Rsync::Get['oss-2.03'] ],
   }
 
   rsync::get { 'iris-csh':
     source => 'duplicon.nmt.edu::Jessie-iris/iris.csh',
     path => '/etc/profile.d/',
     times => true,
-    require => [ Rsync::Get['cwp'], Rsync::Get['oss'] ],
+    require => [ Rsync::Get['cwp-44r5'], Rsync::Get['oss-2.03'] ],
   }
 
 }
