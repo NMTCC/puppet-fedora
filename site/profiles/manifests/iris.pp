6d91ca1e7ba091e13cded056e247f674f76ce654
dgraham <dgraham@nmt.edu>
2017-05-24 16:24:24 -0600

    start merging two IRIS manifests

--- site/profiles/manifests/iris.pp
+++ site/profiles/manifests/iris.pp
@@ -17,12 +17,50 @@ class profiles::iris {
 
       package { $perl: provider => 'apt', ensure => 'installed', }
 
+      rsync::get { 'cwp-44r5':
+        source => 'duplicon.nmt.edu::Jessie-iris/cwp-44r5',
+        path => '/usr/local/',
+        recursive => true,
+        links => true,
+        times => true,
+      }
+
+      rsync::get { 'oss-2.03':
+        source => 'duplicon.nmt.edu::Jessie-iris/oss-2.03',
+        path => '/usr/local/',
+        recursive => true,
+        links => true,
+        times => true,
+      }
+
+      rsync::get { 'xmax-2.0.7':
+        source    => 'duplicon.nmt.edu::Jessie-iris/xmax-2.0.7',
+        path      => '/usr/local/',
+        recursive => true,
+        links     => true,
+        times     => true,
+      }
+
       rsync::get { 'sac':
         source    => 'duplicon.nmt.edu::Jessie-iris/sac-101.6a',
         path      => '/usr/local/',
         recursive => true,
         links     => true,
         times     => true,
+      }
+
+      rsync::get { 'iris-sh':
+        source => 'duplicon.nmt.edu::Jessie-iris/iris.sh',
+        path => '/etc/profile.d/',
+        times => true,
+        require => [ Rsync::Get['cwp-44r5'], Rsync::Get['oss-2.03'] ],
+      }
+
+      rsync::get { 'iris-csh':
+        source => 'duplicon.nmt.edu::Jessie-iris/iris.csh',
+        path => '/etc/profile.d/',
+        times => true,
+        require => [ Rsync::Get['cwp-44r5'], Rsync::Get['oss-2.03'] ],
       }
 
       rsync::get { 'sac-sh':
--- site/profiles/manifests/nmt/rsync/jessie/iris.pp
+++ site/profiles/manifests/nmt/rsync/jessie/iris.pp
@@ -6,48 +6,10 @@ class profiles::nmt::rsync::jessie::iris {
     onlyif  => 'test -e /usr/local/cwp',
   }
 
-  rsync::get { 'cwp-44r5':
-    source => 'duplicon.nmt.edu::Jessie-iris/cwp-44r5',
-    path => '/usr/local/',
-    recursive => true,
-    links => true,
-    times => true,
-  }
-
   exec { 'oss':
     path    => '/bin:/usr/bin',
     command => 'rm -rf /usr/local/oss',
     onlyif  => 'test -e /usr/local/oss',
-  }
-
-  rsync::get { 'oss-2.03':
-    source => 'duplicon.nmt.edu::Jessie-iris/oss-2.03',
-    path => '/usr/local/',
-    recursive => true,
-    links => true,
-    times => true,
-  }
-
-  rsync::get { 'xmax-2.0.7':
-    source    => 'duplicon.nmt.edu::Jessie-iris/xmax-2.0.7',
-    path      => '/usr/local/',
-    recursive => true,
-    links     => true,
-    times     => true,
-  }
-
-  rsync::get { 'iris-sh':
-    source => 'duplicon.nmt.edu::Jessie-iris/iris.sh',
-    path => '/etc/profile.d/',
-    times => true,
-    require => [ Rsync::Get['cwp-44r5'], Rsync::Get['oss-2.03'] ],
-  }
-
-  rsync::get { 'iris-csh':
-    source => 'duplicon.nmt.edu::Jessie-iris/iris.csh',
-    path => '/etc/profile.d/',
-    times => true,
-    require => [ Rsync::Get['cwp-44r5'], Rsync::Get['oss-2.03'] ],
   }
 
 }
