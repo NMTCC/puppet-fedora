512bb39f5ec9ae9bb8ead4b27b0e4a7d28674154
dgraham <dgraham@nmt.edu>
2017-05-24 15:47:15 -0600

    update IRIS manifest

--- site/profiles/manifests/iris.pp
+++ site/profiles/manifests/iris.pp
@@ -4,44 +4,46 @@
 
 class profiles::iris {
 
-  package { 'perl-Moose':
-    ensure   => installed,
-    provider => 'yum',
-  }
+  $perl = [
+    'libconfig-simple-perl',
+    'libshell-perl',
+    'libmoose-perl',
+    'perl-tk',
+  ]
 
+  case $::hostname {
 
-  # Rsync CWPSU
-  rsync::get { '/usr/local/cwp':
-    path   => '/usr/local/',
-    source => 'update.nmt.edu::CWP/cwpsu',
-  }
+    /^speare5/: {
 
-  #Rsync CWPSU profiles
-  rsync::get { '/etc/profile.d/cwpsu.sh':
-    source => 'update.nmt.edu::CWP/cwpsu.sh',
-  }
-  rsync::get { '/etc/profile.d/cwpsu.csh':
-    source => 'update.nmt.edu::CWP/cwpsu.csh',
-  }
+      package { $perl: provider => 'apt', ensure => 'installed', }
 
+      rsync::get { 'sac':
+        source    => 'duplicon.nmt.edu::Jessie-iris/sac-101.6a',
+        path      => '/usr/local/',
+        recursive => true,
+        links     => true,
+        times     => true,
+      }
 
-  # Rsync IRIS Scripts
-  rsync::get { '/usr/local/sh':
-    path   => '/usr/local/',
-    source => 'update.nmt.edu::IRIS/sh',
-  }
-  rsync::get { '/usr/local/pl':
-    path   => '/usr/local/',
-    source => 'update.nmt.edu::IRIS/pl',
-  }
+      rsync::get { 'sac-sh':
+        source  => 'duplicon.nmt.edu::Jessie-iris/sac.sh',
+        path    => '/etc/profile.d/',
+        times   => true,
+        require => Rsync::Get['sac'],
+      }
 
-  #Rsync IRIS Script profiles
-  rsync::get { '/etc/profile.d/iris.sh':
-    source => 'update.nmt.edu::IRIS/iris.sh',
-  }
-  rsync::get { '/etc/profile.d/iris.csh':
-    source => 'update.nmt.edu::IRIS/iris.csh',
-  }
+      rsync::get { 'sac-csh':
+        source  => 'duplicon.nmt.edu::Jessie-iris/sac.csh',
+        path    => '/etc/profile.d/',
+        times   => true,
+        require => Rsync::Get['sac'],
+      }
 
+    }
+    default: {
+      warning('No IRIS configuration for this workstation.')
+    }
+
+  }
 
 }
