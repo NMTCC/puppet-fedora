69add0868d1ab1cd073e4fa633133c14f06366fa
dgraham <dgraham@nmt.edu>
2017-07-24 17:17:27 -0600

    create motd

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,18 +1,35 @@
 # Configure basic stuff
 class profile::base {
 
-  File { '/usr/local/libexec':
+  file { '/usr/local/libexec':
     ensure => directory,
     owner  => 'root',
     group  => 'staff',
     mode   => '2775',
   }
 
-  File { '/usr/local/share/applications':
+  file { '/usr/local/share/applications':
     ensure => directory,
     owner  => 'root',
     group  => 'staff',
     mode   => '2775',
+  }
+
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => '/fs/tcc/motd/motd',
+    require => Service['autofs'],
+  }
+
+  file { '/usr/bin/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => "#!/bin/sh\ncat /fs/tcc/motd/motd\n",
   }
 
 }
