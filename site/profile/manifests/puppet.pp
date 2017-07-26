accfdf387a67de1b36cc0ed424bf78f97cfb691f
dgraham <dgraham@nmt.edu>
2017-07-26 17:55:58 -0600

    setup puppet

---
+++ site/profile/manifests/puppet.pp
@@ -0,0 +1,59 @@
+# Configure puppet
+class profile::puppet {
+
+  if $::chroot {
+
+    service { 'puppet':
+      ensure => stopped,
+      enable => true,
+    }
+
+  }
+  else {
+
+    service { 'puppet':
+      ensure  => running,
+      enable  => true,
+      require => [
+        Ini_setting['caserver'],
+        Ini_setting['puppetserver'],
+        Ini_setting['environment'],
+        Ini_setting['splay'],
+      ],
+    }
+
+  }
+
+  ini_setting {
+
+    'caserver':
+      ensure  => present,
+      path    => '/etc/puppet/puppet.conf',
+      section => 'main',
+      setting => 'ca_server',
+      value   => 'puppet4-server.nmt.edu',;
+
+    'puppetserver':
+      ensure  => present,
+      path    => '/etc/puppet/puppet.conf',
+      section => 'agent',
+      setting => 'server',
+      value   => 'puppet4-linux.nmt.edu',;
+
+    'environment':
+      ensure  => present,
+      path    => '/etc/puppet/puppet.conf',
+      section => 'agent',
+      setting => 'environment',
+      value   => 'production',;
+
+    'splay':
+      ensure  => present,
+      path    => '/etc/puppet/puppet.conf',
+      section => 'agent',
+      setting => 'splay',
+      value   => true,;
+
+  }
+
+}
--- site/role/manifests/stretch.pp
+++ site/role/manifests/stretch.pp
@@ -11,6 +11,7 @@ class role::stretch {
   include profile::localtime
   include profile::kerberos
   include profile::packages
+  include profile::puppet
   include profile::scripts
   include profile::services
   include profile::sshd
