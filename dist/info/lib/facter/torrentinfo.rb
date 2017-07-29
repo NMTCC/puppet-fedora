9a29320b6f2b45febf3488fafacd517649b561da
dgraham <dgraham@nmt.edu>
2017-07-28 20:19:12 -0600

    silence transmission errors

--- dist/info/lib/facter/torrentinfo.rb
+++ dist/info/lib/facter/torrentinfo.rb
@@ -2,7 +2,7 @@ Facter.add(:torrentsactive) do
   confine :kernel => "Linux"
 
   setcode do
-    torrents = `transmission-remote -t all -i | awk '/Hash/ { print $2 }'`
+    torrents = `pgrep transmission-daemon && transmission-remote -t all -i | awk '/Hash/ { print $2 }'`
     result = torrents.split()
   end
 
