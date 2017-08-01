e55412a9d081b373189780edd45364fec0d49879
dgraham <dgraham@nmt.edu>
2017-07-31 19:09:49 -0600

    rsync extlinux

---
+++ site/profile/manifests/rsync/extlinux.pp
@@ -0,0 +1,48 @@
+class profile::rsync::extlinux {
+
+  file { '/boot/extlinux':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/boot/extlinux/extlinux.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    source  => '',
+    require => File['/boot/extlinux'],
+  }
+
+  rsync::get { 'extlinux':
+    source    => "duplicon.nmt.edu::${::lsbdistcodename}-extlinux",
+    path      => '/boot/extlinux/',
+    recursive => true,
+    links     => true,
+    times     => true,
+    require   => File['/boot/extlinux/extlinux.conf'],
+  }
+
+  exec { 'extlinux-modules':
+    provider => shell,
+    command  => 'rsync -aqc /usr/lib/syslinux/modules/bios/ /boot/extlinux/',
+    unless   => 'exit $(rsync -avnc /usr/lib/syslinux/modules/bios/ /boot/extlinux/ | grep c32 | wc -l)',
+    require  => [
+      File['/boot/extlinux/extlinux.conf'],
+      Package['syslinux-common'],
+    ],
+  }
+
+  exec { 'memdisk':
+    provider => shell,
+    command  => 'rsync -aqc /usr/lib/syslinux/memdisk /boot/extlinux/',
+    unless   => 'exit $(diff /usr/lib/syslinux/memdisk /boot/extlinux/memdisk 2>&1 | wc -l)',
+    require  => [
+      File['/boot/extlinux/extlinux.conf'],
+      Package['syslinux-common'],
+    ],
+  }
+
+}
