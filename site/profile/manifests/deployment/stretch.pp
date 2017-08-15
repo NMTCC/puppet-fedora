32a7599def9a8476271eaba1102bd3a8374d05aa
dgraham <dgraham@nmt.edu>
2017-08-15 06:39:39 -0600

    Fall2017 template

--- site/profile/manifests/deployment/stretch.pp
+++ site/profile/manifests/deployment/stretch.pp
@@ -2,21 +2,21 @@
 class profile::deployment::stretch {
 
   $oldtemplates = [
-    'Spring2017-master.2',
+    'Spring2017-master.3',
   ]
 
   $oldtorrents = [
-    '71d1caa7d8a882ecfd3c75db7775fd24b3a2f408',
+    '1f75bbcc246fac018212141c14c47b28cd8073ef',
   ]
 
   # deployment configuration
-  $semester = 'Spring2017'
+  $semester = 'Fall2017'
   $branch = 'master'
-  $build = '3'
+  $build = '1'
   $release = "${semester}-${branch}.${build}"
-  $torrent = '1f75bbcc246fac018212141c14c47b28cd8073ef'
+  $torrent = '24470d057aaa87beece9b3d450e6fd79ba1d30f4'
   $preseed = 'http://duplicon.nmt.edu/preseed/stretch'
-  $syncpre = 'http://template.nmt.edu/images/Spring2017'
+  $syncpre = 'http://template.nmt.edu/images/Fall2017'
 
   profile::deployment::rmtemplate { $oldtemplates: }
   profile::deployment::rmtorrent { $oldtorrents: }
